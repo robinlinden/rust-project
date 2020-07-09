c5a32757f984ee9c85754b116a0db1443a90db6d
Robin Linden <dev@robinlinden.eu>
2020-07-09 17:03:23 +0200

    Create a message box using the Windows API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::ffi::CString;
+use std::os::raw::{c_char, c_int, c_uint, c_void};
+use std::ptr::null_mut;
+
+type UINT = c_uint;
+type HWND = *mut c_void;
+type LPCSTR = *const c_char;
+
+#[link(name = "user32")]
+extern "system" {
+    pub fn MessageBoxA(hWnd: HWND, lpText: LPCSTR, lpCaption: LPCSTR, uType: UINT) -> c_int;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let text = CString::new("Hello, world").unwrap();
+    let caption = CString::new("Great title").unwrap();
+    unsafe {
+        MessageBoxA(
+            null_mut(),
+            text.as_c_str().as_ptr(),
+            caption.as_c_str().as_ptr(),
+            0,
+        );
+    }
 }
