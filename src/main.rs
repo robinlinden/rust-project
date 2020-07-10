e0aaf6c01f9272508224a67ac7ccc00877b58577
Robin Linden <dev@robinlinden.eu>
2020-07-10 17:37:27 +0200

    Support Linux as well

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
-use std::ffi::CString;
-use std::os::raw::{c_char, c_int, c_uint, c_void};
-use std::ptr::null_mut;
+#[cfg(windows)]
+use {
+    std::ffi::CString,
+    std::os::raw::{c_char, c_int, c_uint, c_void},
+    std::ptr::null_mut,
+};
 
+#[cfg(windows)]
 type UINT = c_uint;
+#[cfg(windows)]
 type HWND = *mut c_void;
+#[cfg(windows)]
 type LPCSTR = *const c_char;
 
+#[cfg(windows)]
 #[link(name = "user32")]
 extern "system" {
     pub fn MessageBoxA(hWnd: HWND, lpText: LPCSTR, lpCaption: LPCSTR, uType: UINT) -> c_int;
 }
 
-fn main() {
-    let text = CString::new("Hello, world").unwrap();
-    let caption = CString::new("Great title").unwrap();
+#[cfg(windows)]
+fn message_box(title: String, caption: String) {
+    let title = CString::new(title.into_bytes()).unwrap();
+    let caption = CString::new(caption.into_bytes()).unwrap();
     unsafe {
         MessageBoxA(
             null_mut(),
-            text.as_c_str().as_ptr(),
             caption.as_c_str().as_ptr(),
+            title.as_c_str().as_ptr(),
             0,
         );
     }
+}
+
+#[cfg(not(windows))]
+fn message_box(title: String, caption: String) {
+    println!("{}", title.as_str());
+    println!("{}", caption.as_str());
+}
+
+fn main() {
+    message_box(String::from("Great title"), String::from("Hello world"));
 }
