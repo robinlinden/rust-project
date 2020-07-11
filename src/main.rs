6b7fdd5d9c6d5c005a89ecf031feb1aa5557116e
Robin Linden <dev@robinlinden.eu>
2020-07-11 17:15:04 +0200

    Add a yes/no dialog

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,47 @@ fn message_box(title: String, caption: String) {
     println!("{}", caption.as_str());
 }
 
+#[cfg(windows)]
+fn yes_no_dialog(title: String, caption: String, yes_cb: impl Fn() -> (), no_cb: impl Fn() -> ()) {
+    let title = CString::new(title.into_bytes()).unwrap();
+    let caption = CString::new(caption.into_bytes()).unwrap();
+    let res = unsafe {
+        MessageBoxA(
+            null_mut(),
+            caption.as_c_str().as_ptr(),
+            title.as_c_str().as_ptr(),
+            4,
+        )
+    };
+
+    if res == 7 {
+        no_cb()
+    } else if res == 6 {
+        yes_cb()
+    }
+}
+
+#[cfg(not(windows))]
+fn yes_no_dialog(title: String, caption: String, yes_cb: impl Fn() -> (), no_cb: impl Fn() -> ()) {
+    println!("{}", title.as_str());
+    println!("{}", caption.as_str());
+
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let ans = input.chars().nth(0);
+    if ans == Some('y') || ans == Some('Y') {
+        yes_cb()
+    } else {
+        no_cb()
+    }
+}
+
 fn main() {
     message_box(String::from("Great title"), String::from("Hello world"));
+    yes_no_dialog(
+        String::from("Do you like boxes?"),
+        String::from("y/n?"),
+        || println!("yes pressed"),
+        || println!("no pressed"),
+    );
 }
