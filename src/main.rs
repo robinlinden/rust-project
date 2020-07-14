43b76f0fa087de7c94e8b5c3d44b020c4732868f
Robin Linden <dev@robinlinden.eu>
2020-07-14 20:43:25 +0200

    Allow registering classes on Windows
    This is part of adding support for creating windows.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ mod ui;
 use ui::*;
 
 fn main() {
+    #[cfg(windows)]
+    {
+        register_a_class();
+    }
+
     message_dialog("Great title", "Hello world");
     yes_no_dialog(
         "Do you like boxes?",
--- src/ui.rs
+++ src/ui.rs
@@ -1,8 +1,15 @@
+#![allow(bad_style)]
+
 #[cfg(windows)]
 use {
     std::ffi::CString,
-    std::os::raw::{c_char, c_int, c_uint, c_void},
+    std::os::raw::{c_char, c_ushort, c_int, c_uint, c_void},
     std::ptr::null_mut,
+};
+
+#[cfg(target_pointer_width = "32")]
+use {
+    std::os::raw::{c_long},
 };
 
 #[cfg(windows)]
@@ -11,11 +18,98 @@ type UINT = c_uint;
 type HWND = *mut c_void;
 #[cfg(windows)]
 type LPCSTR = *const c_char;
+#[cfg(windows)]
+type WORD = c_ushort;
+#[cfg(windows)]
+type ATOM = WORD;
+
+#[cfg(all(windows, target_pointer_width = "64"))]
+type __u64 = u64;
+
+#[cfg(all(windows, target_pointer_width = "32"))]
+type UINT_PTR = c_uint;
+#[cfg(all(windows, target_pointer_width = "64"))]
+type UINT_PTR = __u64;
+
+#[cfg(all(windows, target_pointer_width = "32"))]
+type LONG_PTR = c_long;
+#[cfg(all(windows, target_pointer_width = "64"))]
+type LONG_PTR = __u64;
+
+#[cfg(windows)]
+type LPARAM = LONG_PTR;
+#[cfg(windows)]
+type WPARAM = UINT_PTR;
+#[cfg(windows)]
+type LRESULT = LONG_PTR;
+#[cfg(windows)]
+type PVOID = *mut c_void;
+#[cfg(windows)]
+type HANDLE = PVOID;
+#[cfg(windows)]
+type HINSTANCE = HANDLE;
+#[cfg(windows)]
+type HICON = HANDLE;
+#[cfg(windows)]
+type HBRUSH = HANDLE;
+#[cfg(windows)]
+type HCURSOR = HICON;
+#[cfg(windows)]
+type WNDPROC = extern fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT;
+
+#[cfg(windows)]
+#[repr(C)]
+struct tagWNDCLASSEXA {
+    cbSize: UINT,
+    style: UINT,
+    lpfnWndProc: WNDPROC,
+    cbClsExtra: c_int,
+    cbWndExtra: c_int,
+    hInstance: HINSTANCE,
+    hIcon: HICON,
+    hCursor: HCURSOR,
+    hbrBackground: HBRUSH,
+    lpszMenuName: LPCSTR,
+    lpszClassName: LPCSTR,
+    hIconSm: HICON,
+}
+#[cfg(windows)]
+type WNDCLASSEXA = tagWNDCLASSEXA;
 
 #[cfg(windows)]
 #[link(name = "user32")]
 extern "system" {
-    pub fn MessageBoxA(hWnd: HWND, lpText: LPCSTR, lpCaption: LPCSTR, uType: UINT) -> c_int;
+    fn MessageBoxA(hWnd: HWND, lpText: LPCSTR, lpCaption: LPCSTR, uType: UINT) -> c_int;
+    fn RegisterClassExA(Arg1: *const WNDCLASSEXA) -> ATOM;
+}
+
+#[cfg(windows)]
+extern fn wnd_proc(hWnd: HWND, msg: UINT, wParam: WPARAM, lParam: LPARAM) -> LRESULT {
+    println!("{} {} {} {}", hWnd as u64, msg, wParam, lParam);
+    0
+}
+
+#[cfg(windows)]
+pub fn register_a_class() {
+    let window_class = WNDCLASSEXA {
+        cbSize: std::mem::size_of::<WNDCLASSEXA>() as u32,
+        style: 0,
+        lpfnWndProc: wnd_proc,
+        cbClsExtra: 0,
+        cbWndExtra: 0,
+        hInstance: null_mut(),
+        hIcon: null_mut(),
+        hCursor: null_mut(),
+        hbrBackground: null_mut(),
+        lpszMenuName: null_mut(),
+        lpszClassName: null_mut(),
+        hIconSm: null_mut(),
+    };
+
+    unsafe {
+        RegisterClassExA(&window_class);
+        println!("Registered class!")
+    }
 }
 
 #[cfg(windows)]
