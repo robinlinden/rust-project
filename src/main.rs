c4f5d26677d247be03156915312defc82e2d7952
Robin Linden <dev@robinlinden.eu>
2020-07-18 14:35:40 +0200

    Create a simple event loop

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,21 @@ use ui::*;
 fn main() {
     #[cfg(windows)]
     {
-        window_test();
+        println!("Running event system");
+        let mut system = EventLoop::new();
+        let mut running = true;
+        while running {
+            match system.poll_event() {
+                Some(Event::QuitRequested) => {
+                    println!("Got quit!");
+                    running = false;
+                }
+                None => {
+                    println!("Yielded!");
+                    std::thread::sleep(std::time::Duration::from_millis(500));
+                }
+            }
+        }
     }
 
     message_dialog("Great title", "Hello world");
--- src/ui.rs
+++ src/ui.rs
@@ -155,8 +155,46 @@ extern "C" fn wnd_proc(h_wnd: HWND, msg: UINT, w_param: WPARAM, l_param: LPARAM)
     unsafe { DefWindowProcA(h_wnd, msg, w_param, l_param) }
 }
 
-#[cfg(windows)]
-pub fn window_test() {
+pub enum Event {
+    QuitRequested,
+}
+
+use std::collections::VecDeque;
+
+pub struct EventLoop {
+    events: VecDeque<Event>,
+}
+
+impl EventLoop {
+    pub fn new() -> EventLoop {
+        make_test_window();
+
+        EventLoop {
+            events: VecDeque::new(),
+        }
+    }
+
+    pub fn poll_event(&mut self) -> Option<Event> {
+        if self.events.is_empty() {
+            let mut msg = std::mem::MaybeUninit::<MSG>::uninit();
+            unsafe {
+                PeekMessageA(msg.as_mut_ptr(), null_mut(), 0, 0, PM_NOREMOVE);
+                let mut msg = msg.assume_init();
+                while PeekMessageA(&mut msg, null_mut(), 0, 0, PM_REMOVE) != 0 {
+                    TranslateMessage(&msg);
+                    DispatchMessageA(&msg);
+                    if msg.message == WM_QUIT {
+                        self.events.push_back(Event::QuitRequested);
+                    }
+                }
+            }
+        }
+
+        self.events.pop_front()
+    }
+}
+
+fn make_test_window() {
     let window_class_name = CString::new("great window").unwrap();
     let window_class = WNDCLASSEXA {
         cbSize: std::mem::size_of::<WNDCLASSEXA>() as u32,
@@ -190,20 +228,6 @@ pub fn window_test() {
             instance,
             null_mut(),
         );
-
-        let mut msg = std::mem::MaybeUninit::<MSG>::uninit();
-        PeekMessageA(msg.as_mut_ptr(), null_mut(), 0, 0, PM_NOREMOVE);
-        let mut msg = msg.assume_init();
-        while msg.message != WM_QUIT {
-            while PeekMessageA(&mut msg, null_mut(), 0, 0, PM_REMOVE) != 0 {
-                TranslateMessage(&msg);
-                DispatchMessageA(&msg);
-                if msg.message == WM_QUIT {
-                    println!("Window done!");
-                    break;
-                }
-            }
-        }
     }
 }
 
